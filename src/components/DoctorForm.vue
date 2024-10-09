4548564921e0f941990d0418ebf99d91cb643434
Felipe Arakaki <felipe.arakaki@boasnovasgestao.com>
2024-10-09 14:08:39 -0300

    Created doctors view

---
+++ src/components/DoctorForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ action }} médico</h1>
+
+    <span>{{ doctor.nome }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const action = "Adcionar"
+  const doctor = {
+    nome: NULL,
+    crm: NULL,
+    estado: NULL,
+    situacao: NULL
+  }
+</script>
+
+
+
+
+<style>
+</style>
---
+++ src/views/DoctorView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <H1>Gestor de médicos</H1>
+    <li v-for="doctor in doctors" :key="doctor">
+      {{ doctor.nome }}
+      {{ doctor.crm }}
+      {{ doctor.estado }}
+      {{ doctor.situacao }}
+    </li>
+  </div>
+
+
+
+  <!-- To do: componentizar o form -->
+  <DoctorForm></DoctorForm>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import DoctorForm from '../components/DoctorForm.vue'
+
+
+const doctors = ref(
+  [
+    {
+      nome: "Antônio Silva",
+      crm: 123456,
+      estado: 'SP',
+      situacao: 'Ativo'
+    },
+    {
+      nome: "Juliana Cardos",
+      crm: 222222,
+      estado: 'RJ',
+      situacao: 'Inativo'
+    },
+    {
+      nome: "Helena Couto",
+      crm: 333333,
+      estado: 'BA',
+      situacao: 'Ativo'
+    },
+    {
+      nome: "Eduardo Prado",
+      crm: 444444,
+      estado: 'CE',
+      situacao: 'Inativo'
+    },
+    {
+      nome: "Maria Laura",
+      crm: 555555,
+      estado: 'MA',
+      situacao: 'Ativo'
+    },
+    {
+      nome: "Pedro Arantes",
+      crm: 777777,
+      estado: 'SC',
+      situacao: 'Ativo'
+    }
+  ]
+)
+
+
+</script>
+
+
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
